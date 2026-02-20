01c0e5b503821d799a0f2f2ac17c9eaea55bcad4
MateusFariasS <mateus.fsantos08@gmail.com>
2026-02-20 17:07:38 -0300

    first commit

---
+++ bin/notas.dart
@@ -0,0 +1,50 @@
+import 'dart:io';
+
+void main() {
+  List<String> notas = <String>[];
+
+  getComando();
+  adicionarNota(notas);
+  listarNotas(notas);
+}
+
+String getComando() {
+  print(
+    "DIGITE UM COMANDO : 1 - ADICIONAR NOTA ; 2 - LISTAR NOTAS ; 3 - SAIR ",
+  );
+  List<String> comandos = <String>["1", "2", "3"];
+  String? entrada = "";
+
+  entrada = stdin.readLineSync();
+
+  if (entrada == null || !comandos.contains(entrada)) {
+    print("COMANDO INVÁLIDO");
+    getComando();
+  }
+
+  return entrada!;
+}
+
+List<String> adicionarNota(List<String> notas) {
+  print("ESCREVA UMA NOTA:");
+  String? nota = "";
+
+  nota = stdin.readLineSync();
+
+  if (nota == null || nota.isEmpty) {
+    print("NÃO É POSSÍVEL ADICIONAR NOTA VAZIA!");
+    return adicionarNota(notas);
+  }
+
+  notas.add(nota!);
+
+  return notas;
+}
+
+void listarNotas(List<String> notas) {
+  for (var i = 0; i < notas.length; i++) {
+    print(notas);
+  }
+
+  
+}
